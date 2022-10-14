2db1b480533fa873060d774c2797168eabbf2c1f
unknown <stolyarova.199535@gmail.com>
2022-10-14 18:03:03 +0300

    часть план и десктоп версии

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.navigation__list-navigation');
+const navToggle = document.querySelector('.navigation__button');
+
+navMain.classList.remove('navigation__list-navigation_nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('navigation__list-navigation_closed')) {
+    navMain.classList.remove('navigation__list-navigation_closed');
+    navMain.classList.add('navigation__list-navigation_opened');
+  } else {
+    navMain.classList.add('navigation__list-navigation_closed');
+    navMain.classList.remove('navigation__list-navigation_opened');
+  }
+});
