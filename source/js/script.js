4fa1c920c05b332d611c76e40868a7dea55a91aa
unknown <stolyarova.199535@gmail.com>
2022-10-15 20:16:06 +0300

    селект + слайдер(десктоп+планш)

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,24 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('navigation__list-navigation_opened');
   }
 });
+
+
+const selectSingle = document.querySelector('.form__wrapper');
+const selectSingle_title = selectSingle.querySelector('.form__select-title');
+const selectSingle_labels = selectSingle.querySelectorAll('.form__select-label');
+
+selectSingle_title.addEventListener('click', () => {
+  if ('active' === selectSingle.getAttribute('data-state')) {
+    selectSingle.setAttribute('data-state', '');
+  } else {
+    selectSingle.setAttribute('data-state', 'active');
+  }
+});
+
+for (let i = 0; i < selectSingle_labels.length; i++) {
+  selectSingle_labels[i].addEventListener('click', (evt) => {
+    selectSingle_title.textContent = evt.target.textContent;
+    selectSingle.setAttribute('data-state', '');
+  });
+}
+
